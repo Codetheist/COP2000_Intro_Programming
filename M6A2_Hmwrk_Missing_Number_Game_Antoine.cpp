e957ae02c93b8289bbc4beca69a84163d75dfb40
Codetheist <codetheist@codetheist.com>
2019-11-07 19:14:37 -0500

    Homework Due Nov. 11th

--- M6A2_Hmwrk_Missing_Number_Game_Antoine.cpp
+++ M6A2_Hmwrk_Missing_Number_Game_Antoine.cpp
@@ -10,101 +10,50 @@ using namespace std;
 /*randomNum = ( rand( ) % ( maxValue – minValue + 1 ) ) + minValue; */
 
 void instructions();
-
 void displayRandomBoard(int[]);
-
 int beginGame(int[]);
+bool testWinner(int[], int[]);
 
-bool testWinner(int[],
-int[]);
+int main() {
 
+  int user_input = 0;
+  int board_selected = -1;
 
+  instructions();
 
-
-
-int main()
-
-{
-
-int user_input =
-0;
-
-int board_selected = -1;
-
-
-instructions();
-
-board_selected = beginGame();
-
+  board_selected = beginGame();
 
 // do below inside of loop
-
-displayBoard(//need to pass an array here);
-
-
-
+displayBoard(/*need to pass an array here*/);
 
 // get user's answer and store in variable
-
-testWinner(//see HW description);
-
-
-
-
-
-
-
-
+testWinner(/*see HW description*/);
 
 }
 
+void instructions() {
 
-
-void instructions()
-
-{
-
-cout << "instructions";
+  cout << "instructions";
 
 }
 
+void displayBoard(int board[][3]) {
 
+  // i am passed a randomly chosen board and output it to the screen nicely
 
-void displayBoard(int board[][3])
-
-{
-
-// i am passed a randomly chosen board and output it to the screen nicely
-
-// formatted (display a blank or question mark for missing value)
+  // formatted (display a blank or question mark for missing value)
 
 }
 
+int beginGame(int playedBoards[]) {
 
-
-int beginGame(int playedBoards[])
-
-{
-
-/*
-
-This function's only purpose is to choose randomly a previously-unchosen board (by looking at the arg passed to it and selecting randomly again when needed.)
-
-
-Once that unique value has been chosen, use the return statement to notify the rest of the program.
-
-
-
-*/
+  /* This function's only purpose is to choose randomly a previously-unchosen board (by looking at the arg passed to it and selecting randomly again when needed.)
+  Once that unique value has been chosen, use the return statement to notify the rest of the program.*/
 
 }
 
+bool testWinner() {
 
-
-bool testWinner()
-
-{
-
-
+  //
 
 }
