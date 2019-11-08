47ae147cbc6bb5b88777f8a10e715c22cf9b7c18
Codetheist <codetheist@codetheist.com>
2019-11-07 19:13:33 -0500

    Homework Due Nov. 11th

---
+++ M6A2_Hmwrk_Missing_Number_Game_Antoine.cpp
@@ -0,0 +1,110 @@
+//Antoine Gustave
+//Race Results Program
+//Date: 10/12/19
+//Class: Tuesday 6:30pm-9pm
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/*randomNum = ( rand( ) % ( maxValue – minValue + 1 ) ) + minValue; */
+
+void instructions();
+
+void displayRandomBoard(int[]);
+
+int beginGame(int[]);
+
+bool testWinner(int[],
+int[]);
+
+
+
+
+
+int main()
+
+{
+
+int user_input =
+0;
+
+int board_selected = -1;
+
+
+instructions();
+
+board_selected = beginGame();
+
+
+// do below inside of loop
+
+displayBoard(//need to pass an array here);
+
+
+
+
+// get user's answer and store in variable
+
+testWinner(//see HW description);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+void instructions()
+
+{
+
+cout << "instructions";
+
+}
+
+
+
+void displayBoard(int board[][3])
+
+{
+
+// i am passed a randomly chosen board and output it to the screen nicely
+
+// formatted (display a blank or question mark for missing value)
+
+}
+
+
+
+int beginGame(int playedBoards[])
+
+{
+
+/*
+
+This function's only purpose is to choose randomly a previously-unchosen board (by looking at the arg passed to it and selecting randomly again when needed.)
+
+
+Once that unique value has been chosen, use the return statement to notify the rest of the program.
+
+
+
+*/
+
+}
+
+
+
+bool testWinner()
+
+{
+
+
+
+}
