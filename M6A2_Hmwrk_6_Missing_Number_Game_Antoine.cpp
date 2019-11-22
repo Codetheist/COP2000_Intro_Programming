87d5bcbe8c578fc151e8f71b059a47bd65b43d05
Codetheist <codetheist@codetheist.com>
2019-11-21 19:28:20 -0500

    Homework Due Nov. 26th (Extended Homework)

--- M6A2_Hmwrk_6_Missing_Number_Game_Antoine.cpp
+++ M6A2_Hmwrk_6_Missing_Number_Game_Antoine.cpp
@@ -4,10 +4,10 @@
 //Class: Tuesday 6:30pm-9pm
 
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
+#include <vector>
 using namespace std;
-
-/*randomNum = ( rand( ) % ( maxValue – minValue + 1 ) ) + minValue; */
 
 void instructions();
 void displayRandomBoard(int[]);
@@ -16,6 +16,10 @@ bool testWinner(int[], int[]);
 
 int main() {
 
+  const int ROW = 3, COL =4;
+
+  randomNum = ( rand( ) % ( maxValue  minValue + 1 ) ) + minValue;
+
   int user_input = 0;
   int board_selected = -1;
 
@@ -23,17 +27,22 @@ int main() {
 
   board_selected = beginGame();
 
-// do below inside of loop
-displayBoard(/*need to pass an array here*/);
+  // do below inside of loop
+  displayBoard(/*need to pass an array here*/);
 
-// get user's answer and store in variable
-testWinner(/*see HW description*/);
+  // get user's answer and store in variable
+  testWinner(/*see HW description*/);
 
 }
 
 void instructions() {
 
-  cout << "instructions";
+  cout << "*****************************************************************" << endl;
+  cout << setw(38) << "MISSING NUMBERS GAME" << endl;
+  cout << setw(39) << "A fun brain game..." << endl;
+  cout << setw(62) << "Please Enter a whole number to guess the missing number..." << endl;
+  cout << setw(50) << "Program Developed by: Antoine Gustave" << endl;
+  cout << "*****************************************************************" << endl;
 
 }
 
@@ -47,10 +56,10 @@ void displayBoard(int board[][3]) {
 
 int beginGame(int playedBoards[]) {
 
-  /* This function's only purpose is to choose randomly a previously-unchosen board (by looking at the arg passed to it and selecting randomly again when needed.)
+  /*This function's only purpose is to choose randomly a previously-unchosen board (by looking at the arg passed to it and selecting randomly again when needed.)
   Once that unique value has been chosen, use the return statement to notify the rest of the program.*/
 
-}
+//}
 
 bool testWinner() {
 
